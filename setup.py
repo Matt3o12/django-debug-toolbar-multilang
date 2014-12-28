ae1b40f8b506acb6eb2a1353e5855c4ca482e87f
Matteo Kloiber <info@matt3o12.de>
2014-12-28 22:11:50 +0100

    Added README.rst for PyPi.

--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
 from setuptools import setup
 import sys
 
-extra_install_requires = []
-if sys.version_info < (3, 3):
-    extra_install_requires.append("mock")
 
 def get_long_description():
-    with open('README.md', "rt") as file:
+    with open('README.rst', "rt") as file:
         return file.read()
+
+requirements = []
+if sys.version_info < (3, 3):
+    requirements.append("mock")
+
+requirements.append("Django>=1.7.1")
+requirements.append("django-debug-toolbar")
 
 setup(
     name='django-debug-toolbar-multilang',
     version='1.0',
     description='Language panel for django-debug-toolbar',
     long_description=get_long_description(),
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md',
     url='http://github.com/matt3o12/django-debug-toolbar-multilang',
     author='Matteo Kloiber',
     author_email='info@matt3o12.de',
     license='MIT',
     packages=["debug_toolbar_multilang"],
-    install_requires = ["Django>=1.7.1", "django-debug-toolbar"] + extra_install_requires,
+    install_requires=requirements
 )
