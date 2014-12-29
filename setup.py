4d10404c0f972c9c4c492c7b639820a45e7469be
Matteo Kloiber <info@matt3o12.de>
2014-12-29 01:23:11 +0100

    Added package data files (e.g. translations and templates).

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 
@@ -15,14 +15,15 @@ requirements.append("django-debug-toolbar")
 
 setup(
     name='django-debug-toolbar-multilang',
-    version='1.0',
+    version='1.0.1',
     description='Language panel for django-debug-toolbar',
     long_description=get_long_description(),
     url='http://github.com/matt3o12/django-debug-toolbar-multilang',
     author='Matteo Kloiber',
     author_email='info@matt3o12.de',
     license='MIT',
-    packages=["debug_toolbar_multilang"],
+    include_package_data=True,
+    packages=find_packages(exclude=('tests.*', 'tests')),
     install_requires=requirements,
     classifiers=[
         "Framework :: Django",
@@ -30,10 +31,12 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Natural Language :: German",
+        "Operating System :: OS Independent"
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.2",
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
 
-    ]
+    ],
+    zip_safe=False
 )
